0570712189b803a0dbacb95dc942b536f2588cc2
shakib8080 <mdshakibulhasan0808@gmail.com>
2024-07-09 18:01:27 +0600

    first commit

---
+++ concept-checklist.js
@@ -0,0 +1,139 @@
+// 1 write a 3 variable
+
+// let firstnumber = 20;
+// let myname = 'shakib';
+// let friends = ['shakib','ashik',' rakib'];
+
+// 2 changeable varible (let & var )
+
+// let firsrnumber = 10;
+// let secoundnumber = 30;
+// firsrnumber = past ;
+// let totalls = past +secoundnumber;
+// console.log(totalls);
+
+//2  not changeable vaiable (const) 
+//  const finumber = 10;
+//  const senumber = 20;
+//  let first = finumber;
+//  let secound = senumber;
+//  let totall = finumber +senumber;
+//  console.log(totalls);
+
+// 3 simple math oparation (+ - * / %)
+// (+)
+// let  mynumber = 40;
+// let mymothern = 50;
+// let myfather = 49;
+// let allnumber = myfather+ mymothern+ mynumber;
+// console.log(allnumber);
+
+// (-)
+// let  mynumbers = 40;
+// let mymotherns = 50;
+// let myfathers = 49;
+// let allnumbers = myfathers- mymotherns- mynumbers;
+// console.log(allnumbers);
+
+// (*)
+// let  mynumberss = 40;
+// let mymothernss = 50;
+// let myfatherss = 49;
+// let allnumberss = myfatherss* mymothernss*mynumberss;
+// console.log(allnumberss);
+
+// (/)
+// let totalltk = 180;
+// let person = 6 ;
+// let perpersontk = totalltk / person;
+// console.log(perpersontk);
+
+// (%)
+// let totalltks = 190;
+// let persons = 6 ;
+// let perpersontks = totalltks % persons;
+// console.log(perpersontks);
+
+// 4 comparison (>,<, == ,!= ,<=, >=,)
+// (>)
+// var myage = 22; 
+// var herage = 25;
+// let shakib = myage ;
+// let ashik = herage ;
+// if(shakib > ashik){
+//     console.log(true);
+// }
+// else{
+//     console.log(false);
+// }
+
+// (<)
+// var myage = 22; 
+// var herage = 25;
+// let shakibs = myage ;
+// let ashiks = herage ;
+// if(shakibs < ashiks){
+//     console.log(true);
+// }
+// else{
+//     console.log(false);
+// }
+
+// (==)
+// var myage = 22; 
+// var herage = 25;
+// let shakib = myage ;
+// let ashik = herage ;
+// if(shakib == ashik){
+//     console.log(true);
+// }
+// else{
+//     console.log(false);
+// }
+
+// (!=)
+// var myage = 22; 
+// var herage = 25;
+// let shakib = myage ;
+// let ashik = herage ;
+// if(shakib != ashik){
+//     console.log(true);
+// }
+// else{
+//     console.log(false);
+// }
+
+// 4 conditional comparison
+
+// let haveAjob = true;
+// let haveAcar =false;
+// let education = true;
+// let haveAproperty= false;
+// if(haveAcar && haveAproperty ||education ){
+//     console.log('i am interested');
+// }
+// else{
+//     console.log('i am sorry');
+// }
+
+// 5 whie loop to display 7 to 19 only odd Number
+
+let cound =6 ;
+let i = 0 ;
+while(i < 19){
+
+    if(i %2 == 1){
+        cound= cound+1;
+    }
+    i++
+   
+    
+}
+console.log(cound);
+
+
+
+
+
+
+
